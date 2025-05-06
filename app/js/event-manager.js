1119dadc744258ccdc1c68efcfc21b470bec80ba
Fabien <62819235+Fabien603@users.noreply.github.com>
2025-05-06 21:44:23 +0200

    renderRegularEventsInWeeklyView

--- app/js/event-manager.js
+++ app/js/event-manager.js
@@ -2290,7 +2290,7 @@ export class EventManager {
                 
                 // Trouver la colonne correspondant au jour et à l'heure de début
                 // Nombre de colonnes par rangée = 8 (1 pour les heures + 7 jours)
-                const columnIndex = dayIndex + 7 * startHour + 1; // +1 pour tenir compte de la colonne des heures
+                const columnIndex = dayIndex * startHour + 1; // +1 pour tenir compte de la colonne des heures
                 
                 const dayColumn = Array.from(dayColumns).find((col, index) => 
                     index % 8 === dayIndex + 1 && // +1 pour tenir compte de la colonne des heures
